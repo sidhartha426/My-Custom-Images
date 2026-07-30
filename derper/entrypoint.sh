b473cab22f04837fcd0ddc89fd1a6d0230a097fb
Sidhartha Sankar <sidharthasankarswain425@gmail.com>
2026-07-30 17:37:08 +0530

    derper v1.98.9 improved for ipv6

--- derper/entrypoint.sh
+++ derper/entrypoint.sh
@@ -2,7 +2,7 @@
 exec derper -hostname "${DERP_DOMAIN}" \
         -certmode manual \
         -certdir /caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/${DERP_DOMAIN} \
-        -a :443 \
+        -a "[::]:8443" \
         -stun \
         -stun-port 34479 \
         -verify-client-url "${VERIFY_URL}" \
