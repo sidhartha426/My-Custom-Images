3cdc2ef52d83bc563b987abf720562281c1a3cc1
Sidhartha Sankar <sidharthasankarswain425@gmail.com>
2026-08-02 11:55:27 +0530

    derper v1.98.9 improved entry script

--- derper/entrypoint.sh
+++ derper/entrypoint.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
+
+# Check if DERP_DOMAIN is set and not empty
+if [ -z "${DERP_DOMAIN}" ]; then
+    echo "Error: DERP_DOMAIN environment variable is missing."
+    exit 1
+fi
+
+# Check if VERIFY_URL is set and not empty
+if [ -z "${VERIFY_URL}" ]; then
+    echo "Error: VERIFY_URL environment variable is missing."
+    exit 1
+fi
+
+# Announce startup details
+echo "Derper server is running on domain: ${DERP_DOMAIN}"
+echo "Verification server is running at: ${VERIFY_URL}"
+
+# Execute derper
 exec derper -hostname "${DERP_DOMAIN}" \
         -certmode manual \
-        -certdir /caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/${DERP_DOMAIN} \
+        -certdir "/caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/${DERP_DOMAIN}" \
         -a "[::]:8443" \
         -stun \
         -stun-port 34479 \
         -verify-client-url "${VERIFY_URL}" \
         -verify-client-url-fail-open=false
+
